afd9770e15ecb75b651f05990019834b08880d13
Ricael <ricaelmenezes@gmail.com>
2025-08-13 12:57:42 -0300

    feat: endpoints on ControllerNinjas to make a crud

--- src/main/java/br/com/ricaeldurand/CadastroDeNinjas/Ninjas/Controller/ControllerNinjas.java
+++ src/main/java/br/com/ricaeldurand/CadastroDeNinjas/Ninjas/Controller/ControllerNinjas.java
@@ -1,8 +1,6 @@
 package br.com.ricaeldurand.CadastroDeNinjas.Ninjas.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,40 @@ public class ControllerNinjas {
     @GetMapping("/inicio")
     public String boasVindas(){
         return "Minha primeira mensagem na rota";
+    }
+
+    //adicionar ninja
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "NinjaCriado";
+    }
+
+    //Mostrar todos ninjas
+    @GetMapping("/mostrartodos")
+    public String mostrarTodosNinjas(){
+        return "Ninjas mostrados";
+    }
+
+    //Mostrar ninjas por id
+    @GetMapping("/mostrarId")
+    public String mostrarPorId(){
+        return "Ninja por id mostrado";
+    }
+    //Alterar dados dos ninjas
+    @PutMapping("/alterarId")
+    public String alterarNinjaporId(){
+        return "Alterar ninja por id";
+    }
+    @PutMapping("/alterar")
+    public String alterarNinja(){
+        return "Alterar ninja";
+    }
+    //procurar ninja
+
+
+    //Deletar ninja
+    @DeleteMapping("/deletar")
+    public String deletarPorId(){
+        return "Ninja deletado por id";
     }
 }
