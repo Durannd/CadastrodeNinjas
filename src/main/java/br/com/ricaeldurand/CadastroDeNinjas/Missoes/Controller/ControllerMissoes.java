d7f12ee73af700f8fb88f68dd5fd83d897d8136f
Ricael <ricaelmenezes@gmail.com>
2025-08-17 02:30:28 -0300

    feat: MissoesService addeds

--- src/main/java/br/com/ricaeldurand/CadastroDeNinjas/Missoes/Controller/ControllerMissoes.java
+++ src/main/java/br/com/ricaeldurand/CadastroDeNinjas/Missoes/Controller/ControllerMissoes.java
@@ -1,14 +1,17 @@
 package br.com.ricaeldurand.CadastroDeNinjas.Missoes.Controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/missoes")
 public class ControllerMissoes {
-    @GetMapping("/MissoesNinjas")
-    public String boasVindas(){
-        return "Bem vindo! Voces está nas missoes ninjas.";
+
+    @GetMapping("/listar")
+    public ResponseEntity<?> listar(){
+
     }
+
 }
---
+++ src/main/java/br/com/ricaeldurand/CadastroDeNinjas/Missoes/Services/MissoesService.java
@@ -0,0 +1,54 @@
+package br.com.ricaeldurand.CadastroDeNinjas.Missoes.Services;
+
+import br.com.ricaeldurand.CadastroDeNinjas.Missoes.Model.MissoesModel;
+import br.com.ricaeldurand.CadastroDeNinjas.Missoes.Repository.MissoesRepository;
+import br.com.ricaeldurand.CadastroDeNinjas.Missoes.dto_mapper.MissoesDTO;
+import br.com.ricaeldurand.CadastroDeNinjas.Missoes.dto_mapper.MissoesMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class MissoesService {
+    private MissoesRepository missoesRepository;
+    private MissoesMapper missoesMapper;
+
+    public MissoesService(MissoesRepository missoesRepository, MissoesMapper missoesMapper) {
+        this.missoesRepository = missoesRepository;
+        this.missoesMapper = missoesMapper;
+    }
+
+    public List<MissoesDTO> listarMissoes() {
+        List<MissoesModel> missoes = missoesRepository.findAll();
+        return missoes.stream()
+                .map(missoesMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    public MissoesDTO listarPorId(Long id) {
+        Optional<MissoesModel> missoesModel = missoesRepository.findById(id);
+        return missoesModel.map(missoesMapper::map).orElse(null);
+    }
+
+    public MissoesDTO addMissao(MissoesDTO missoesDTO) {
+        MissoesModel missoesModel = missoesMapper.map(missoesDTO);
+        missoesModel = missoesRepository.save(missoesModel);
+        return missoesMapper.map(missoesModel);
+    }
+
+    public void deletarMissao(Long id) {
+        missoesRepository.deleteById(id);
+    }
+
+    public MissoesDTO atualizarPorId(Long id, MissoesDTO missoesDTO) {
+        if (missoesRepository.existsById(id)) {
+            MissoesModel missoesModel = missoesMapper.map(missoesDTO);
+            missoesModel.setId(id);
+            MissoesModel missaoSalva = missoesRepository.save(missoesModel);
+            return missoesMapper.map(missaoSalva);
+        }
+        return null;
+    }
+}
